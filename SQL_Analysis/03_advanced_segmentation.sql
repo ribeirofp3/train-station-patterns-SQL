0413920a557863c154ec35ecce38febdadd0f503
Fabrizio <ribeirofp3@gmail.com>
2026-04-02 21:54:37 +0200

    Create 03_advanced_segmentation.sql

---
+++ SQL_Analysis/03_advanced_segmentation.sql
@@ -0,0 +1,20 @@
+-- Segmentation of Milan's trips (Short, Medium, Long)
+-- Used Window Functions to calculate the participation percentage
+WITH MilanTrips AS (
+    SELECT 
+        t.duration_minutes,
+        CASE 
+            WHEN t.duration_minutes < 20 THEN 'Short'
+            WHEN t.duration_minutes BETWEEN 20 AND 60 THEN 'Medium'
+            ELSE 'Long'
+        END AS duration_category
+    FROM Trips t
+    JOIN Stations s ON s.station_id = t.start_station_id
+    WHERE s.city = 'Milano'
+)
+SELECT 
+    duration_category,
+    COUNT(*) AS count_trips,
+    ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER(), 2) AS pct_of_total
+FROM MilanTrips
+GROUP BY duration_category;
