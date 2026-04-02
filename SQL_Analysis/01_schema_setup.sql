2f769205f0b248c731289b882e2453168ea315aa
Fabrizio <ribeirofp3@gmail.com>
2026-04-02 21:50:07 +0200

    Update 01_schema_setup.sql

--- SQL_Analysis/01_schema_setup.sql
+++ SQL_Analysis/01_schema_setup.sql
@@ -1,1 +1,14 @@
+-- Creating tables
+CREATE TABLE Stations (
+    station_id INT PRIMARY KEY,
+    station_name VARCHAR(100),
+    city VARCHAR(50)
+);
 
+CREATE TABLE Trips (
+    trip_id INT PRIMARY KEY,
+    start_station_id INT,
+    duration_minutes INT,
+    trip_date DATE,
+    FOREIGN KEY (start_station_id) REFERENCES Stations(station_id)
+);
