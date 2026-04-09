243a99a8d8166025a41e0ff2bd32491813a79346
Fabrizio <ribeirofp3@gmail.com>
2026-04-09 21:03:54 +0200

    Update 02_operational_analysis.sql

--- SQL_Analysis/02_operational_analysis.sql
+++ SQL_Analysis/02_operational_analysis.sql
@@ -1,17 +1,12 @@
--- High-Traffic Cities: Where is the highest traffic ?
-SELECT 
-    s.city, 
-    COUNT(t.trip_id) AS total_trips
-FROM Stations s 
-JOIN Trips t ON s.station_id = t.start_station_id
-GROUP BY s.city
-ORDER BY total_trips DESC;
-
 -- Trip Outliers: Which trips last longer than the global average?
-
-SELECT trip_id, duration_minutes
-FROM Trips
-WHERE duration_minutes > (
-    SELECT AVG(duration_minutes) 
+WITH TripDurations AS (
+    SELECT 
+        trip_id,
+        DATEDIFF(MINUTE, trip_start_time, trip_end_time) AS duration_minutes
     FROM Trips
-);
+    WHERE trip_status <> 'Cancelled' -- Exclude cancelled trips from average
+)
+SELECT trip_id, duration_minutes
+FROM TripDurations
+WHERE duration_minutes > (SELECT AVG(duration_minutes) FROM TripDurations)
+ORDER BY duration_minutes DESC;
